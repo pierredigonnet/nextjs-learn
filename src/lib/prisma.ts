81b0aa8f48d6657e5e383e4648f1d6e7a37b5499
Pierre DIGONNET <pierre.digonnet@reconnect.fr>
2025-10-28 10:44:55 +0100

    feat: start config for deployment (next : update prod prisma db schemas ??)

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,8 +1,19 @@
 // lib/prisma.ts
 import { PrismaClient } from "@/generated/prisma";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+const getPrisma = () => {
+  const client = new PrismaClient();
+
+  if (process.env.NODE_ENV === "development") {
+    return client;
+  }
+
+  return client.$extends(withAccelerate());
+};
+
+export const prisma = globalForPrisma.prisma || getPrisma();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
