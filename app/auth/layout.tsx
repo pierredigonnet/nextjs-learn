0302b9e329447bfe9e8cf1a4ed1b559b18834b42
Pierre DIGONNET <pierre.digonnet@reconnect.fr>
2025-10-29 09:52:21 +0100

    fix: changer props type in auth layout

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,6 +1,6 @@
 import { PageLayout } from "@/components/layout";
 import type { PropsWithChildren } from "react";
 
-export default function Layout(props: { children: PropsWithChildren }) {
+export default function Layout(props: PropsWithChildren) {
   return <PageLayout>{props.children}</PageLayout>;
 }
