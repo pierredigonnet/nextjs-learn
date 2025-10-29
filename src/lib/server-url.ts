87851b296600425a11eef4ec7b57edeb3c1ff0dd
Pierre DIGONNET <pierre.digonnet@reconnect.fr>
2025-10-29 09:43:24 +0100

    "add prisma and server url"

--- src/lib/server-url.ts
+++ src/lib/server-url.ts
@@ -3,5 +3,13 @@ export const getServerUrl = () => {
     return "http://localhost:3000";
   }
 
+  if (process.env.VERCEL_ENV === "production") {
+    return "https://nextjs-learn-eight-roan-54.vercel.app";
+  }
+
+  if (process.env.VERCEL_ENV === "preview") {
+    return process.env.VERCEL_URL;
+  }
+
   return "https://prod.com";
 };
